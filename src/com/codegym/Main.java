57964ab1b59a0005e828bab13f5302cec1e37a8a
huongtran99 <huongtrannd1999@gmail.com>
2021-08-18 21:37:51 +0700

    first commit

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,10 @@
+package com.codegym;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MyLinkedList myLinkedList = new MyLinkedList(2);
+        myLinkedList.printList();
+        System.out.println(myLinkedList.getNumNodes());
+    }
+}
